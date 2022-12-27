5a28892b9430fbf54d43d527ffecfe0eee92cdee
abuamar142 <amarjabon130@gmail.com>
2022-12-27 18:25:46 +0700

    dokumentasi materi toTheMoon

---
+++ android/app/src/main/kotlin/com/example/to_the_moon/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.to_the_moon
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+import 'package:to_the_moon/dokumentasi_materi.dart';
+
+main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: MyWidget(),
+    );
+  }
+}
+
+class MyWidget extends StatefulWidget {
+  const MyWidget({super.key});
+
+  @override
+  State<MyWidget> createState() => _MyWidgetState();
+}
+
+class _MyWidgetState extends State<MyWidget> {
+  void tambahAngka() {
+    setState(() {
+      angka.add(angka.length + 1);
+    });
+  }
+
+  List angka = [1, 2, 3, 4, 5];
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: ListView.builder(
+        itemBuilder: (BuildContext context, int index) {
+          return Center(
+            child: Text(
+              angka[index].toString(),
+            ),
+          );
+        },
+        itemCount: angka.length,
+      ),
+      floatingActionButton: FloatingActionButton(onPressed: () {
+        tambahAngka();
+      }),
+    );
+  }
+}
